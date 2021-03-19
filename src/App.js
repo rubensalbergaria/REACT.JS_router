c5e9e72b26ba5b3b0c9b4121c9e358362ed82735
Rubens Albergaria <rubensalbergaria@gmail.com>
2021-03-19 14:05:25 -0300

    another update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
-import Login from "./pages/Login"
-import AboutUs from "./pages/AboutUs"
-import { useState } from 'react';
+
+import Login from "./pages/Login";
+import AboutUs from "./pages/AboutUs";
+import Main from "./pages/Main";
+
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Login/>
-     
-    </div>
-  );
+  return (   
+        <Router> 
+          <Route path="/" exact render={(props)=> <Main/>}/>
+          <Route path="/login" render={(props)=> <Login/>}/>
+          <Route path="/aboutus" render={(props)=> <AboutUs/>}/>
+        </Router>   
+    );
 }
 
 export default App;
